1139282492a387ad8d31d22478d8f9affc60dace
timothy.lee <marlboromoo@gmail.com>
2013-10-18 09:36:22 +0800

    Fix usage.

--- karma.py
+++ karma.py
@@ -80,6 +80,6 @@ def karma(bot, trigger):
         karma, reason= get_karma(table, who)
         bot.say("%s: %s, reason: %s" % (who, karma, reason))
     else:
-        bot.say("%skarma <nick> - Reports status for <nick>." % bot.config.prefix.split('\\')[1])
+        bot.say("%skarma <nick> - Reports karma status for <nick>." % bot.config.prefix.split('\\')[1])
 
 
